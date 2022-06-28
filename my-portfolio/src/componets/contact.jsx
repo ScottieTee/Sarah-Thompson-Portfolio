148098eaefa995c6e3b7d9b6f667e4d529b68756
Sarah Thompson <sthompson@fintechprocessing.com>
2022-06-28 11:53:22 -0400

    fixed contact buttons

--- my-portfolio/src/componets/contact.jsx
+++ my-portfolio/src/componets/contact.jsx
@@ -12,11 +12,12 @@ const Contact = () => {
         >
             <div className="pb-8">
                 <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">Contact</p>
-                <p className="text-gray-300 p-4"> You can contact me by email - sscotthompson@gmail.com</p>
+                <p className="text-gray-300 p-4">I would love to connect with you!</p>
             </div>
-            <Link to="projects" smooth={true} duration={700}></Link>
-            <a className="flex justify-between items-center w-full text-gray-300" target="_blank" href="https://www.linkedin.com/in/sarah-thompson-0b9234191/">LinkedIn<FaLinkedin size={30} /></a>
-            <button className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center">LinkedIn</button>
+            <a className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center" target="_blank" href="https://www.linkedin.com/in/sarah-thompson-0b9234191/">LinkedIn<FaLinkedin size={30} /></a>
+            <a className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center" target="_blank" href="https://www.linkedin.com/in/sarah-thompson-0b9234191/">LinkedIn<FaLinkedin size={30} /></a>
+            <a className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center" target="_blank" href="https://www.linkedin.com/in/sarah-thompson-0b9234191/">LinkedIn<FaLinkedin size={30} /></a>
+            <a className="text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-3 my-8 mx-auto flex items-center" target="_blank" href="https://www.linkedin.com/in/sarah-thompson-0b9234191/">LinkedIn<FaLinkedin size={30} /></a>
         </div>
     );
   };
